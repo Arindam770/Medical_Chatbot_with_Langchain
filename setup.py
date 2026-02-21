5ee84df944d8f6869e16ce4ccc7713ede6890244
Arindam Mondal <arindam03215@gmail.com>
2026-02-21 13:02:54 +0530

    Adding Knowledge sources and research file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arindam Mondal",
+    author_email="arindam03215@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "flask",
+        "sentence-transformers",
+        "pypdf",
+        "python-dotenv",
+        "langchain-openai",
+        "langchain-pinecone",
+        "langchain-community",
+    ]
+)
